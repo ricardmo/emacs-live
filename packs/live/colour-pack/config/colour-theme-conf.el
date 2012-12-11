9b5f987c0825c2cb26a82d91f9a655f37df84add
ricja <marco.ricard@gmail.com>
2012-12-10 20:37:07 -0500

    Adds solarized color theme

--- packs/live/colour-pack/config/colour-theme-conf.el
+++ packs/live/colour-pack/config/colour-theme-conf.el
@@ -4,6 +4,7 @@
 ;; use blackbored colour theme
 (load-file (concat (live-pack-lib-dir) "cyberpunk.el"))
 (load-file (concat (live-pack-lib-dir) "gandalf.el"))
+(load-file (concat (live-pack-lib-dir) "emacs-color-theme-solarized/color-theme-solarized.el"))
 
-(color-theme-cyberpunk)
+(color-theme-solarized-dark)
 (set-cursor-color "yellow")
